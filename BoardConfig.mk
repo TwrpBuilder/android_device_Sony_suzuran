ce8c9733f6d4cf8b821e746c0d6278ec5d2b4034
androidlover5842 <androidlover5842@gmail.com>
2018-02-13 19:21:34 +0530

    initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2018 The TwrpBuilder Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/Sony/suzuran
+
+TARGET_BOARD_PLATFORM := msm8994
+TARGET_BOOTLOADER_BOARD_NAME := suzuran
+
+# Recovery
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 15810560 
+BOARD_FLASH_BLOCK_SIZE := 1000000
+
+
+# Kernel
+TARGET_PREBUILT_KERNEL := device/Sony/suzuran/kernel
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 lpm_levels.sleep_disabled=1 boot_cpus=0-5 msm_rtb.filter=0x3F ehci-hcd.park=3 dwc3.maximum_speed=high dwc3_msm.prop_chg_detect=Y coherent_pool=8M sched_enable_power_aware=1 user_debug=31 androidboot.hardware=suzuran buildvariant=userdebug androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
+
+include device/generic/twrpbuilder/BoardConfig64.mk
